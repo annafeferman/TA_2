73c8e307f4cf376ca984126bf05327b40d87ec8c
annafeferman <61123869+annafeferman@users.noreply.github.com>
2020-11-02 13:49:15 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
